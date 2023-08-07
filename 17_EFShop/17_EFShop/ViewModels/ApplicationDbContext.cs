dd12db60e47dca82aa54cfd134f1af870b31810b
IrinaSviridova <sviridovairis@gmail.com>
2023-08-07 17:35:18 +0300

    17. Правки логгирования

--- 17_EFShop/17_EFShop/ViewModels/ApplicationDbContext.cs
+++ 17_EFShop/17_EFShop/ViewModels/ApplicationDbContext.cs
@@ -9,39 +9,38 @@ namespace _17_EFShop.ViewModels
 	//затем в конструкторе идет обращение в конструктору базового класса DbContext с параметром options
 	class ApplicationDbContext : DbContext
 	{
-		private static readonly StreamWriter logStream = new StreamWriter("log.txt", true);
+		private static readonly StreamWriter logStream = new StreamWriter("log.txt", true)
+		{ AutoFlush = true };
 		private static DbContextOptions<ApplicationDbContext> options = ConfigurateApp(
 			"AppJsConfig.json", "DefaultConnection", logStream);
-		
+
 		public DbSet<Client> Clients { get; set; } //!если указать не с-ва, а поля, работать не будет!
 		public DbSet<Shopping> Shoppings { get; set; }
 
 		public ApplicationDbContext() : base(options)
 		{
 			Database.EnsureCreated(); //если БД не создана - создается БД на основе Models		
-		}	
+		}
 
 		//метод возвращает параметры конфигурации на основе ключа из json-файла (значение ключа - строка подключения к БД)
 		private static DbContextOptions<ApplicationDbContext> ConfigurateApp(string configFileName, string keyName, StreamWriter logStreamWriter)
 		{
-			var configurationBuilder = new ConfigurationBuilder();
-			configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
-			configurationBuilder.AddJsonFile(configFileName);
-			var config = configurationBuilder.Build();
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile(configFileName)
+				.Build();
 			var connectionString = config.GetConnectionString(keyName);
-			var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();			
-			var appOptions = optionsBuilder.UseSqlServer(connectionString).Options;
-			//настройки логгирования (только информационного характера) -
-			//запись в поток logStream (в txt-файл, Append=true)
-			optionsBuilder.LogTo(logStreamWriter.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
-			return appOptions;
+			return new DbContextOptionsBuilder<ApplicationDbContext>()
+				.LogTo(logStreamWriter.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+				.UseSqlServer(connectionString)
+				.Options;
 		}
 
 		//для закрытия потока logStreamWriter переопределяем Dispose()
 		public override void Dispose()
-		{
-			base.Dispose();
+		{			
 			logStream.Dispose();
+			base.Dispose();
 		}
 	}
 }
